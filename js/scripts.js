fe5bcf57cf16862b4ea360f283a12f0630e2bb60
unknown <lyam35,25@gmail.com>
2021-12-02 11:45:30 -0500

    cambios script

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,54 @@
-String.prototype.replaceAt=function(index, character) { return this.substr(0, index) + character + this.substr(index+character.length); } 
+String.prototype.replaceAt = function (index, character) {
+    return (
+      this.substr(0, index) + character + this.substr(index + character.length)
+    );
+  };
+  
+  const palabras = ["desarrollador", "programador", "javascript", "ahorcado"];
+  
+  const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+  let palabraConGuiones = palabra.replace(/./g, "_ ");
+  let contadorFallos = 0;
+  
+  
+  document.querySelector("#output").innerHTML = palabraConGuiones;
+  
+  document.querySelector("#volver").addEventListener("click", () => {
+    window.location.reload()
 
-
-const palabras = ['oso','isa','usa','luz','ojo'];
-const palabra = palabras[Math.floor(Math.random()*palabras.length)];
-let guiones = palabra.replace(/./g,"_ ");
-let contadorFallos = 0
-
-document.querySelector('#output').innerHTML = guiones;
-document.querySelector('#verificar').addEventListener('click',() => {
-const letra = document.querySelector('#letra').value;
-
-let fallo = true;
-for(const i in palabra){
-    if(letra == palabra[i]){
-        guiones = guiones.replaceAt(i*2, letra);
-        fallo = false;
+  })
+  
+  document.querySelector("#volver2").addEventListener("click", () => {
+    window.location.reload()
+  })
+  
+  document.querySelector("#verificar").addEventListener("click", () => {
+    
+    const letra = document.querySelector("#letra").value;
+    let haFallado = true;
+    for (const i in palabra) {
+      if (letra == palabra[i]) {
+        palabraConGuiones = palabraConGuiones.replaceAt(i * 2, letra);
+        haFallado = false;
+      }
     }
-}
-
-if(fallo){
-contadorFallos++;
-document.querySelector('#ahorcado').style.backgroundPosition = -(230*contadorFallos) + 'px 0';
-if(contadorFallos==6){
-    alert("Game Over")
-}
-}else{
-    if(guiones.indexOf('_')<0){
-       document.querySelector('#ganaste').style.display = 'flex';
+  
+    if(haFallado){
+      contadorFallos++;
+      document.querySelector('#ahorcado').style.
+      backgroundPosition = -(182*contadorFallos) + 'px 0'
+      if(contadorFallos == 5){
+        document.querySelector('#perdiste').style.display = 'flex';
+      }
     }
-}
-document.querySelector('#output').innerHTML = guiones;
-
-document.querySelector('#letra').value = '';
-document.querySelector('#letra').focus();
-});
+    else{
+        if(palabraConGuiones.indexOf('_') <0){
+            document.querySelector('#ganaste').style.display = 'flex';
+            
+        }
+    }
+  
+    document.querySelector("#output").innerHTML = palabraConGuiones;
+    document.querySelector('#letra').value = '';
+    document.querySelector('#letra').focus();
+  });
